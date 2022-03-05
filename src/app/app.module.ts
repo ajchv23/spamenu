912d00d65f6655f5978f0ce939b8371989c7e13e
Alberto Chaves <ajchv23@gmail.com>
2022-03-04 18:18:06 -0600

    categorias imagenes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+//Rutas
+import { APP_ROUTING } from './app.routes';
+
+
+//Componentes
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { CartComponent } from './components/cart/cart.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
 
 @NgModule({
   declarations: [
@@ -15,10 +22,12 @@ import { CartComponent } from './components/cart/cart.component';
     HomeComponent,
     AboutComponent,
     HeroesComponent,
-    CartComponent
+    CartComponent,
+    ErrorPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
